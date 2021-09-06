4e33c4707b59ba9065e988ab8cee1fd4d302abb1
Philipp Spiertz <strgaltphil@web.de>
2021-09-06 22:29:07 +0200

    Prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,31 @@
 module.exports = {
-    prefix: '',
-    purge: {
-      content: [
-        './src/**/*.{html,ts}',
-      ]
-    },
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-      extend: {
-          animation: {
-              tilt: "tilt 10s infinite linear"
+  prefix: "",
+  purge: {
+    content: ["./src/**/*.{html,ts}"],
+  },
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      animation: {
+        tilt: "tilt 10s infinite linear",
+      },
+      keyframes: {
+        tilt: {
+          "0%, 50%, 100%": {
+            transform: "rotate(0deg)",
           },
-          keyframes: {
-              tilt: {
-                  "0%, 50%, 100%": {
-                      transform: "rotate(0deg)"
-                  },
-                  "25%": {
-                    transform: "rotate(1deg)"
-                },
-                "75%": {
-                    transform: "rotate(-1deg)"
-                }
-              }
-          }
+          "25%": {
+            transform: "rotate(1deg)",
+          },
+          "75%": {
+            transform: "rotate(-1deg)",
+          },
+        },
       },
     },
-    variants: {
-      extend: {},
-    },
-    plugins: [require('@tailwindcss/forms'),require('@tailwindcss/typography')],
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
